6f3a3b00a8ec34ee60f17738fd093f1e01818557
AnikoSchmidt <aniko@antanaconsult.hu>
2020-10-08 10:32:30 -0700

    Section 10 JS Stalker Exercise

---
+++ Aniko_JS_StalkerExercise.js
@@ -0,0 +1,5 @@
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?");
+let yourAge = prompt("How old are you?");
+console.log("Your name is " + firstName + " " +lastName);
+console.log("You are " + yourAge + " years old.");
