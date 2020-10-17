5063d9e22104958bdbd7f5f0909c108e05227f2b
AnikoSchmidt <aniko@antanaconsult.hu>
2020-10-16 18:51:43 -0700

    Section 15 DOM Selector Exercise

---
+++ Aniko_DOM_SelectorExercise.js
@@ -0,0 +1,8 @@
+//Select the first paragraph on four different ways
+
+let firstP = document.getElementById("first");
+let firstP2 = document.querySelector("#first");
+let firstP3 = document.querySelector(".special");
+let firstP4 = document.querySelectorAll("#first");
+let firstP5 = document.querySelector("p");
+let firstP6 = document.getElementsByTagName("p");
