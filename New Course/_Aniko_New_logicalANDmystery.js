77b4d11b29b92d8d9f2969d56ec242e091074405
AnikoSchmidt <aniko@antanaconsult.hu>
2020-10-21 23:31:09 -0700

    Section 16 Logical AND Mystery

---
+++ New Course/_Aniko_New_logicalANDmystery.js
@@ -0,0 +1,6 @@
+const mystery = 'Puz7le'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+
+// LEAVE THIS CODE ALONE! (pretty please)
+if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
+    console.log("YOU GOT IT!!!");
+}
