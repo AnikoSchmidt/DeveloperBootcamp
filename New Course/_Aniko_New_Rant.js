792aea5cf947cb7ffbd1159687bd93160bde6b5f
AnikoSchmidt <aniko@antanaconsult.hu>
2020-10-23 15:51:52 -0700

    Section 20 Rant Exercise

---
+++ New Course/_Aniko_New_Rant.js
@@ -0,0 +1,8 @@
+// DEFINE YOUR FUNCTION:
+function rant(message) {
+    let myMessage = message.toUpperCase();
+    console.log(myMessage);
+    console.log(myMessage);
+    console.log(myMessage);
+}
+rant('I hate beets');
