4b97a42b7d011ef47afc299cb6d72793b54ad2b7
AnikoSchmidt <aniko@antanaconsult.hu>
2020-10-23 16:32:59 -0700

    Section 20 Multiple Arguments Exercise

---
+++ New Course/_Aniko_New_MultipleArguments.js
@@ -0,0 +1,12 @@
+// define isSnakeEyes below:
+function isSnakeEyes(die1, die2) {
+    if (die1 === 1 && die2 ===1) {
+        console.log('Snake Eyes!')
+    } else {
+        console.log('Not Snake Eyes!')
+    }
+}
+
+isSnakeEyes (1,1)
+isSnakeEyes (1,5)
+isSnakeEyes (4,5)
